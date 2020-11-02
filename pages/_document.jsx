ff79d6eb3e5985da18ff95cc89cecc167e972765
KASHYAPYAN <indiawebdesign.seo@gmail.com>
2020-11-02 19:50:47 +0530

    wip

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,14 +10,16 @@ export default class MyDocument extends Document {
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
                     <meta property="og:title" content="Gitartha Kalita" />
-                    <meta property="og:type" content="Website Development" />
+                    <meta property="og:description" content="Product Designer , Product Developer, Entrepreneur" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="http://gitartha-kalita.vercel.app/" />
 
-                    <meta property="og:image" content="https://example.com/ogp.jpg" />
-                    <meta property="og:image:secure_url" content="https://secure.example.com/ogp.jpg" />
-                    <meta property="og:image:type" content="image/jpeg" />
-                    <meta property="og:image:width" content="400" />
-                    <meta property="og:image:height" content="300" />
-                    <meta property="og:image:alt" content="A shiny red apple with a bite taken out" />
+                    {/* <meta property="og:image" content="https://www.site-shot.com/dKldSh0DEeu_FAJCrBEABA" /> */}
+                    <meta property="og:image" content="https://www.site-shot.com/98EeXB0DEeuYXgJCrBEABQ" />                    
+                    <meta property="og:image:type" content="image/png" />
+                    <meta property="og:image:width" content="1200" />
+                    <meta property="og:image:height" content="630" />
+                    <meta property="og:image:alt" content="Gitartha Kalita" />
 
                 </Head>
                 <body>
