830bcde1634d4c941295979f55c866624e7c9568
KASHYAPYAN <indiawebdesign.seo@gmail.com>
2020-11-02 17:00:05 +0530

    wip

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,6 +8,17 @@ export default class MyDocument extends Document {
                 <Head>
                     <meta charSet="utf-8" />
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+                    <meta property="og:title" content="Gitartha Kalita" />
+                    <meta property="og:type" content="Website Development" />
+
+                    <meta property="og:image" content="https://example.com/ogp.jpg" />
+                    <meta property="og:image:secure_url" content="https://secure.example.com/ogp.jpg" />
+                    <meta property="og:image:type" content="image/jpeg" />
+                    <meta property="og:image:width" content="400" />
+                    <meta property="og:image:height" content="300" />
+                    <meta property="og:image:alt" content="A shiny red apple with a bite taken out" />
+
                 </Head>
                 <body>
                     <Main />
--- src/data/SLIDE_DATA.js
+++ src/data/SLIDE_DATA.js
@@ -33,11 +33,14 @@
 
 
 export const SLIDE_DATA = [
-  '/assets/loopd.webp',
+  // '/assets/loopd.webp',
+  '/assets/1.png',
+  '/assets/2.png',
   '/assets/prep2.png',
+  '/assets/3.png',
   '/assets/prep1.png',
-  '/assets/homepage.webp',
-  '/assets/hatch.jpg',
+  // '/assets/homepage.webp',
+  // '/assets/hatch.jpg',
   
   // '/assets/3.jpg',
   // '/assets/4.jpg',
