8b880f091f2e68f025c763d04af7206867907cf9
KASHYAPYAN <indiawebdesign.seo@gmail.com>
2020-11-17 21:38:33 +0530

    wip

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,15 +8,23 @@ export default class MyDocument extends Document {
                 <Head>
                     <meta charSet="utf-8" />
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
                     <meta property="og:title" content="Gitartha Kalita" />
-                    <meta property="og:description" content="Product Designer , Product Developer, Entrepreneur" />
+                    <meta property="og:description" content="Product Designer ,Product Developer, Entrepreneur" />
                     <meta property="og:type" content="website" />
                     <meta property="og:url" content="http://gitartha-kalita.vercel.app/" />
 
-                    {/* <meta property="og:image" content="https://www.site-shot.com/dKldSh0DEeu_FAJCrBEABA" /> */}
-                    <meta property="og:image" content="https://www.site-shot.com/cached_image/98EeXB0DEeuYXgJCrBEABQ" />                    
-                    
+                    <meta property="og:image" content="https://www.site-shot.com/cached_image/dKldSh0DEeu_FAJCrBEABA" />
+                    {/* <meta property="og:image" content="https://www.site-shot.com/cached_image/98EeXB0DEeuYXgJCrBEABQ" />*/}
+
+                    <meta name="twitter:card" content="summary_large_image"></meta>
+                    <meta property="twitter:domain" content="gitartha-kalita.vercel.app"></meta>
+                    <meta property="twitter:url" content="https://gitartha-kalita.vercel.app/"></meta>
+                    <meta name="twitter:title" content="Gitartha Kalita"></meta>
+                    <meta name="twitter:description" content="Product Designer , Product Developer, Entrepreneur"></meta>
+                    <meta name="twitter:image" content="https://www.site-shot.com/cached_image/98EeXB0DEeuYXgJCrBEABQ"></meta>
+
 
                 </Head>
                 <body>
--- src/data/SLIDE_DATA.js
+++ src/data/SLIDE_DATA.js
@@ -1,43 +1,9 @@
-// export const SLIDE_DATA = [
-//     {
-//         slide_id: "s101",
-//         slide_image_url:"/assets/loopd.webp",
-//         slide_title: "Loopd",
-//         slide_link_url:"http://gitartha-kalita.vercel.app"
-//     },
-//     {
-//         slide_id: "s102",
-//         slide_image_url:"/assets/prep2.png",
-//         slide_title: "Prepademia APSC",
-//         slide_link_url:"http://gitartha-kalita.vercel.app"
-//     },
-//     {
-//         slide_id: "s103",
-//         slide_image_url:"/assets/prep1.png",
-//         slide_title: "Prepademia",
-//         slide_link_url:"http://gitartha-kalita.vercel.app"
-//     },
-//     {
-//         slide_id: "s104",
-//         slide_image_url:"/assets/homepage.webp",
-//         slide_title: "Homepage",
-//         slide_link_url:"http://gitartha-kalita.vercel.app"
-//     },
-//     {
-//         slide_id: "s105",
-//         slide_image_url:"/assets/hatch.jpg",
-//         slide_title: "Loopd",
-//         slide_link_url:"http://gitartha-kalita.vercel.app"
-//     },
-// ]
-
-
 export const SLIDE_DATA = [
   // '/assets/loopd.webp',
   '/assets/1.png',
-  '/assets/2.png',
+  // '/assets/2.png',
   '/assets/prep2.png',
-  '/assets/3.png',
+  // '/assets/3.png',
   '/assets/prep1.png',
   // '/assets/homepage.webp',
   // '/assets/hatch.jpg',
